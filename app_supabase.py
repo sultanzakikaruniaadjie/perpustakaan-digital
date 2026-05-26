3f2ef4166e2809206664754bd7c36f64648b8e16
sultanzakikaruniaadjie <sultanzakikaruniaadjie@gmail.com>
2026-05-26 17:24:10 +0700

    Update app_supabase.py

--- app_supabase.py
+++ app_supabase.py
@@ -9,7 +9,7 @@ import streamlit as st
 from supabase import create_client, Client
 from datetime import datetime, timedelta
 
-st.set_page_config(page_title="Perpustakaan Digital", page_icon="📚",
+st.set_page_config(page_title="Perpustakaan Digital Politeknik META", page_icon="📚",
                    layout="wide", initial_sidebar_state="expanded")
 
 st.markdown("""
@@ -75,7 +75,7 @@ def halaman_login():
     st.markdown("<br>", unsafe_allow_html=True)
     _, col, _ = st.columns([1,1.2,1])
     with col:
-        st.markdown("## 📚 Perpustakaan Digital")
+        st.markdown("## 📚 Perpustakaan Digital Politeknik Meta")
         st.markdown("**Kelompok 2 — Sultan Zaki & Zulfa**")
         st.markdown("---")
         tab1, tab2 = st.tabs(["🔑 Login","📝 Daftar Akun"])
@@ -114,7 +114,7 @@ def halaman_login():
 # ── SIDEBAR ──────────────────────────────────────────────────────
 def render_sidebar():
     with st.sidebar:
-        st.markdown("## 📚 Perpustakaan"); st.markdown("---")
+        st.markdown("## 📚 Perpustakaan PoltekMETA"); st.markdown("---")
         role=st.session_state.current_role; nama=st.session_state.current_nama
         st.markdown(f"**{nama}**")
         st.markdown(f'<span class="badge-{"admin" if role=="admin" else "peminjam"}">● {role.capitalize()}</span>', unsafe_allow_html=True)
